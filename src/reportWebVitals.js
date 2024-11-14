f643d6d71a1b08d7e9d35e59b7b3866869faabb7
Umut Can YILMAZ <umutcyilmaz@192.168.1.12>
2024-11-14 14:42:17 +0300

    initial commit

---
+++ src/reportWebVitals.js
@@ -0,0 +1,12 @@
+import { getCLS, getFID, getLCP, getTTFB } from 'web-vitals';
+
+const reportWebVitals = (onPerfEntry) => {
+  if (onPerfEntry && onPerfEntry instanceof Function) {
+    getCLS(onPerfEntry);
+    getFID(onPerfEntry);
+    getLCP(onPerfEntry);
+    getTTFB(onPerfEntry);
+  }
+};
+
+export default reportWebVitals;
